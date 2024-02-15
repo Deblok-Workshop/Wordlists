36cbf3ee84ee288c0f2978825c0a99b9b54fad91
rare1k <hi@rare1k.dev>
2024-02-14 19:26:22 -0500

    Create rockyou.sh

---
+++ rockyou.sh
@@ -0,0 +1,6 @@
+# Download rockyou.txt.gz
+curl -o rockyou.txt.gz https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz?ref_type=heads
+if [ "$1" != "--gunzip" ]; then
+  gunzip rockyou.txt.gz
+  rm rockyou.txt.gz
+fi
